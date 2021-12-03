88a2b48ec33f4798b4bbfceab9bf847137848633
Hunter Thompson <aatman.bos@smallcase.com>
2021-12-03 15:48:45 +0530

    chore: remove

--- cdk8sredissentinel/internal/types.go
+++
@@ -1,7 +0,0 @@
-package internal
-import (
-	"github.com/cdk8s-team/cdk8s-core-go/cdk8s"
-	"github.com/aws/constructs-go/constructs/v3"
-)
-type Type__cdk8sApiObject = cdk8s.ApiObject
-type Type__constructsConstruct = constructs.Construct
